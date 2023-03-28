eb860fb7bb13f70a5120e1441ff2e83fe0e15a9d
mhemon <mhemon02@gmail.com>
2023-03-29 00:54:29 +0600

    show cart data from local storage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,9 +4,14 @@ import './Cart.css'
 const Cart = ({cart}) => {
     let totalPrice = 0
     let shipping = 0
+    let quantity = 0
     for(const product of cart){
-        totalPrice += product.price
-        shipping += product.shipping
+        if(product.quantity === 0){
+            product.quantity = 1
+        }
+        totalPrice += product.price * product.quantity
+        shipping += product.shipping * product.quantity
+        quantity += product.quantity
     }
 
     const tax = totalPrice*7/100
@@ -14,7 +19,7 @@ const Cart = ({cart}) => {
     return (
         <div className='cart'>
             <h3 style={{textAlign:'center', paddingTop:'28px', marginTop:'0'}}>Order Summary</h3>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Shipping: ${shipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
